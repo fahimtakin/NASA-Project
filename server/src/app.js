5062a82aa99e88b7d785a5ca73114bf20d7eb7b9
fahimtakin <fahim.tanzil@hotmail.com>
2023-05-10 10:14:34 +0600

    Add request logging middleware Morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(
   })
 );
 
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
