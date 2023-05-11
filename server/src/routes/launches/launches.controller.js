4d3e68ce330cdb44b2e777456ba213cae6e7d8c9
fahimtakin <fahim.tanzil@hotmail.com>
2023-05-11 23:15:44 +0600

    POST launches validation.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const { addNewLaunch, getAllLaunches } = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
@@ -6,7 +7,22 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+  if (
+    !launch.mission ||
+    !launch.destination ||
+    !launch.rocket ||
+    !launch.launchDate
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property.",
+    });
+  }
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date.",
+    });
+  }
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
